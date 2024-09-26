545729392bebb8bc2faccb9ab8afdf5f8fbca1b2
gustavozequim <gustavozequim9@gmail.com>
2024-09-25 23:31:54 -0300

    Adiciona audio da música

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const nomeMusica = document.getElementById('nome-musica');
-const musica = document.getElementById('musica');
-const play = document.getElementById('botao-play');
+const nomeMusica = document.getElementById("nome-musica");
+const musica = document.getElementById("musica");
+const play = document.getElementById("botao-play");
 
 
-nomeMusica.innerText = 'Não dá pra continuar';
-musica.pause();
+nomeMusica.innerText = 'Doce Luar';
+musica.play();
+musica.pause()
 
 function tocarMusica() {
     play.querySelector('.bi').classList.remove('bi bi-play-circle-fill');
     play.querySelector('.bi').classList.add('bi bi-pause-circle-fill');
-    musica.play();
 }
 
 play.addEventListener('click', tocarMusica);
